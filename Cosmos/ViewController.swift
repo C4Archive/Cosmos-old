6964b40756c31bf5638b8c70666a2050c7a94b38
-- C4 -- <info@c4ios.com>
2015-07-27 20:38:39 -0700

    Removes InfiniteScrollview Chapter code from WorkSpace

--- Cosmos/ViewController.swift
+++ Cosmos/ViewController.swift
@@ -22,74 +22,7 @@ import C4
 
 class WorkSpace: C4CanvasController {
 
-    let infiniteScrollView = InfiniteScrollView() //create a new InfiniteScrollview object
     override func setup() {
-        //creates CGRect from canvas frame
-        //sets frame of infiniteScrollView to match canvas
-        infiniteScrollView.frame = CGRect(canvas.frame)
-
-        //add the view to the canvas
-        canvas.add(infiniteScrollView)
-
-
-        addVisualIndicators()
-    }
-
-
-    func addVisualIndicators() {
-        //the max number of indicators
-        let count = 20
-
-        //the gap between indicators
-        let gap = 150.0
-
-        //initial offset because we're positioning from the center of each indicator's view
-        let dx = 40.0
-
-        //the calculated total width of the view's contentSize
-        let width = Double(count + 1) * gap + dx
-
-        //create main indicators
-        for x in 0...count {
-            //create a center point for the new indicator
-            let point = C4Point(Double(x) * gap + dx, canvas.center.y)
-            //create a new indicator
-            createIndicator("\(x)", at: point)
-        }
-
-        //create additional indicators
-        var x : Int = 0
-
-        //create an offset variable
-        var offset = dx
-
-        //The total width (including the last "view" of the infiniteScrollView is based on the width + screen width
-        //So, the total width and count of how many "extra" indicators to add is somewhat arbitrary
-        //This is why we use a while loop
-
-        //while the offset is less than the view's width
-        while offset < Double(infiniteScrollView.frame.size.width) {
-            //create a center point whose x value starts is the total width + the current offset
-            let point = C4Point(width + offset, canvas.center.y)
-            //create the width
-            createIndicator("\(x)", at: point)
-            //increase the offset for the next point
-            offset += gap
-            //increate x to be used as the variable for the next indicator's number
-            x++
-        }
-
-        //update infiniteScrollView contentSize
-        infiniteScrollView.contentSize = CGSizeMake(CGFloat(width) + infiniteScrollView.frame.size.width, 0)
-    }
-
-    func createIndicator(text: String, at point: C4Point) {
-        //create a textshape
-        let ts = C4TextShape(text: text)
-        //center the shape
-        ts.center = point
-        //add it to the canvas
-        infiniteScrollView.add(ts)
     }
 
     override func prefersStatusBarHidden() -> Bool {
